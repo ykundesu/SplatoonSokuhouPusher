0f27cef7d2ec992caf577b28266e76c1e7d33c29
ykundesu <49261069+ykundesu@users.noreply.github.com>
2023-06-01 19:29:04 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@ self.addEventListener('push', function (event) {
 			badge: 'https://splatoonsokuhouimage.web.fc2.com/PushIcon.png',
 			tag: 'web-push-study-notification',
 			data: {
-				url: 'https://web.dev/push-notifications-overview/',
+				url: 'https://twitter.com/SplatoonSokuhou',
 			},
 		}, ...event.data.json()
 	}
