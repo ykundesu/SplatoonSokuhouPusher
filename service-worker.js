61576b181c156325449e7e28408c46bd5fe3c37b
ykundesu <49261069+ykundesu@users.noreply.github.com>
2023-06-02 22:15:11 +0900

    service-worker.js の更新

--- service-worker.js
+++ service-worker.js
@@ -16,7 +16,7 @@ self.addEventListener('push', function (event) {
 			badge: 'https://splatoonsokuhouimage.web.fc2.com/PushIcon.png',
 			tag: 'web-push-study-notification',
 			data: {
-				url: 'https://twitter.com/SplatoonSokuhou',
+				url: 'twitter://user?screen_name=SplatoonSokuhou',
 			},
 		}, ...event.data.json()
 	}
